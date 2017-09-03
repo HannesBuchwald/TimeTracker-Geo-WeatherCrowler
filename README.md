bcfd9bcf35a64561916e59b9feee0dc15bf0878f
Hannes Buchwald <hb052@hdm-stuttgart.de>
2017-09-03 22:01:29 +0200

    init checkin

---
+++ README.md
@@ -0,0 +1,13 @@
+# [TimeTracker Geo-WeatherCrowler](https://www.timetracker.cc)
+
+## Overview
+
+The TimeTracker GET-WeatherCrowler is script to view get weather data from a weatherstation.
+
+## Team
+
+- Hannes Buchwald ([hannes@timetracker.cc](mailto:hannes@timetracker.cc))
+
+## License
+
+    This script is under the GNU AGPLv3 license
---
+++ assets/js/app.js
@@ -0,0 +1,92 @@
+
+var config = {
+  //Test Json file
+  //geojson: "http://api.wunderground.com/api/e59ee9f2c70cb3a6/conditions/q/pws:IEASTERN104.json"
+
+  // Original Json file
+  geojson: "http://api.wunderground.com/api/e59ee9f2c70cb3a6/geolookup/q/pws:0uxu4sd3.json"
+
+};
+
+// Fetch the GeoJSON file
+$.getJSON(config.geojson, function (data) {
+  var responds;
+
+  try {
+      responds = data.response.error.type;
+  }
+  catch(err) {
+  }
+
+  // If Station is offline print it
+  if(responds == "Station:OFFLINE") {
+    console.log(responds);
+    document.getElementById("field_name").innerHTML = responds + " - no weather data could be saved! Please connect the weatherstation to the internet.";
+  } else {
+
+    console.log(data);
+
+    try {
+      // parse all Data to the new json and send it to the main server
+      var newData =  {
+                 "type": "Feature",
+                 "properties": [
+                   {
+                     "id": data.current_observation.station_id,
+                     "status": "Weatherstation",
+                     "created_at": data.current_observation.local_time_rfc822,
+                     "longitude": data.current_observation.observation_location.longitude,
+                     "latitude": data.current_observation.observation_location.latitude,
+                     "marker-color": "blue",
+                     "temperature": data.current_observation.temp_c,
+                     "dew_point": data.current_observation.dewpoint_c,
+                     "humidity": data.current_observation.relative_humidity,
+                     "precipitation": data.current_observation.precip_today_string,
+                     "wind_speed": data.current_observation.wind_kph,
+                     "wind_gust": data.current_observation.wind_gust_kph,
+                     "wind_direction": data.current_observation.wind_dir,
+                     "pressure": data.current_observation.pressure_mb
+                   }
+                 ],
+                 "geometry": [
+                   {
+                     "type": "Point",
+                     "coordinates": [data.current_observation.observation_location.longitude,data.current_observation.observation_location.latitude]
+                   }
+                 ]
+               };
+      console.log(newData);
+
+
+      // send data to server
+      $.ajax({
+          url: 'https://server-timetracker.herokuapp.com/a5c8e07368efde43/status/',
+          type: 'POST',
+          contentType: 'application/json; charset=utf-8',
+          data: JSON.stringify(newData),
+          dataType: 'json',
+          async: false,
+          success: function(msg) {
+              alert(msg);
+              // successfuly saved
+              document.getElementById("field_name").innerHTML = "The Weatherstaion Data where successfuly saved";
+              document.getElementById("field_name").innerHTML = "Latest updated: " + data.current_observation.local_time_rfc822;
+          },
+          error: function(xhr, ajaxOptions, thrownError) {
+              document.getElementById("field_name").innerHTML = "Error while sending";
+              // if (xhr.status == 200) {
+              //
+              //     alert(ajaxOptions);
+              // }
+              // else {
+              //     alert(xhr.status);
+              //     alert(thrownError);
+              // }
+        }
+      });
+    }
+    catch(err) {
+      document.getElementById("field_name").innerHTML = "The Weatherstaion Data where not saved - some error had happedn";
+    }
+}
+});
